12810c61bf1fae1202434bfb63c63e3384035040
Arbab khan <ak2077719@gmail.com>
2022-12-31 19:05:03 +0530

    fixed cors required error

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./routes/routes");
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 
